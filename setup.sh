b13b7c489682d72f0cba0ee1936c8f5c934e20b6
Paul Zeiger <pmz.research@pm.me>
2026-06-13 01:22:26 -0700

    Renamed the jax EELS code to EELSfornax

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 pip install --no-cache-dir -e /workspaces/code/abTEM
 pip install --no-cache-dir -e /workspaces/code/gpaw
 pip install --no-cache-dir -e /workspaces/code/gpaw-weaver
-pip install --no-cache-dir -e /workspaces/code/ouijax
+pip install --no-cache-dir -e /workspaces/code/EELSfornax
 
